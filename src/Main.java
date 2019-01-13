0afe337db507413b8364645d7742336c0127ea1e
Noctambulant666 <b_z_simson@yahoo.co.nz>
2019-01-13 20:50:59 +1300

    Third commit

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		// Test text for learning how to use GitHub
 		System.out.println("Hi, my name is Billy!");
 		System.out.println("My favourite colour is green.");
+		System.out.println("My favourite mode of transportation is by car.");
 	}
 
 }
